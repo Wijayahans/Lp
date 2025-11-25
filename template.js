b62e2bb9817cf49cda651f8524a520ed1fe24939
WIjaya Hans <miohann3@gmail.com>
2025-11-25 22:14:37 +0700

    Update template.js

--- template.js
+++ template.js
@@ -1,3 +1,5 @@
 <div style="width:1px;height:1px;overflow:hidden">
-<p><a href="https://ruscompromat.news/"></a>https://ruscompromat.news/</p>
+<p>
+  <a href="https://ruscompromat.news/">https://ruscompromat.news</a>
+</p>
 </div>
